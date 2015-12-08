2b5ac839883f4484ea9cb3f983e2a22c0dc60094
Ricardo De Jesus <dejesuswork@gmail.com>
2015-12-08 13:40:45 -0500

    Added name to index and show

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151207220712) do
+ActiveRecord::Schema.define(version: 20151208174247) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20151207220712) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
